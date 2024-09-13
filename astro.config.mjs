417b63ff8d72e1a3c53e0976a7d1af5206c9cf14
Bryan Rosas <bryan.rosas.quispe@gmail.com>
2024-09-12 22:32:21 -0500

    fix:corrige archivo de configuracion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
-import github from "@astrojs/github";
 import icon from "astro-icon";
 
 export default defineConfig({
   output: "static",
-  adapter: github(),
   integrations: [
     icon({
       include: {
